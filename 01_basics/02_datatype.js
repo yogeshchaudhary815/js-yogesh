bae9e57d22a389b5f34c648d13a06064a494b920
yogeshchaudhary815 <yogeshchaudhary8150@gmail.com>
2025-06-15 14:45:21 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+myName = "yogesh chaudhary";
+myClass = "B.tech";
+let myFirstCompany;
+myfirstpackage = null;
+console.log(" myName = " + typeof(myName)); 
+// null variable
+console.log("this is secound method to print output");
+console.log(`myfirstpackage = ${typeof myfirstpackage}`);
+// undefined variable
+console.log(`myFirstCompany = ${typeof myFirstCompany}`);
